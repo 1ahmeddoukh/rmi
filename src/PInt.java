2bc8c894029176947196b8b25d9f97aaccbef86f
unknown <ahmeddokh47@gmail.com>
2021-01-03 15:55:16 +0100

    first commit

---
+++ src/PInt.java
@@ -0,0 +1,9 @@
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface PInt extends Remote {
+	 public static int[][] ProduitMatriciel(int[][]A,int[][] B) throws RemoteException {
+		return null;}
+	 }
